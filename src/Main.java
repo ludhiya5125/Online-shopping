1c206e79ec509535214f366621dc361796193a0e
ludhiya5125 <94975450+ludhiya5125@users.noreply.github.com>
2024-04-19 09:37:57 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,10 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         Scanner scanner = new Scanner(System.in);
-
-        // Create some sample products
         Product tv = new Electronics("Smart Phone", 523, 65.99, 9, "Vivo", 12);
         Product shirt = new Clothing("Women's Top", 150, 15.99, 20, "M", "Cotton");
-
-        // Add products to the shopping cart
+      // Add products to the shopping cart
         ShoppingCart cart = new ShoppingCart();
         cart.addToCart(tv);
         cart.addToCart(shirt);
